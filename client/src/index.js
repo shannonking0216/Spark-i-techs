83bd4eac797b116f7ccd8275eb92c8af30326333
Brandon Harris <BrandonMGH@Yahoo.com>
2019-05-03 09:53:37 -0700

    pulling down contact component changes

--- client/src/index.js
+++ client/src/index.js
@@ -12,6 +12,8 @@ import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 import Navbar from './components/Navbar';
 import Main from "./pages/Main"
+// import App from './App'
+
 
 
 // Here is if we have an id_token in localStorage
@@ -25,10 +27,10 @@ ReactDOM.render(
         <div>
             <Navbar />
             <Route exact path="/" component={Main} />
-            {/* <Route exact path="/login" component={App} /> */}
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/profile" component={Profile} />
+              {/* <Route exact path="/login" component={App} /> */}
         </div>
     </Router>
     , document.getElementById('root')
