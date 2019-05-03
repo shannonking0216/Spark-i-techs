82e3d9dd42a77772d96b523e6a9c39ec105024da
Brandon Harris <BrandonMGH@Yahoo.com>
2019-05-03 12:09:06 -0700

    created a working 404 page

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 
 import registerServiceWorker from './registerServiceWorker';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import axios from "axios";
 
 // Our Components
@@ -12,6 +13,7 @@ import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 import Navbar from './components/Navbar';
 import Main from "./pages/Main"
+import ErrorPage from "./pages/Errorpage"
 // import App from './App'
 
 
@@ -26,10 +28,13 @@ ReactDOM.render(
     <Router>
         <div>
             <Navbar />
+            <Switch>
             <Route exact path="/" component={Main} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/profile" component={Profile} />
+            <Route component={ErrorPage} />
+            </Switch>
               {/* <Route exact path="/login" component={App} /> */}
         </div>
     </Router>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,7 +4,6 @@ import API from './../utils/API';
 import { Link } from 'react-router-dom';
 import AddImage from '../components/AddImage';
 import DeleteImage from '../components/DeleteImage';
-import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 
