b612168651545c9bf41bc6aea41ac21106092939
Brandon Harris <BrandonMGH@Yahoo.com>
2019-05-03 21:45:50 -0700

    Copied from Github Shannon's update code for the Carousel gallery.  Carousel Gallery works

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -13,6 +13,24 @@ import FooterLink from "../components/Footer"
 
 
 class Main extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            shownGallery: null
+        };
+    }
+
+    handleImageClick = (targetClass, index) => {
+        if (targetClass === "carousel-inner") {
+            if (index === 0)
+                this.setState({ shownGallery: <NatureGallery /> })
+            else if (index === 1)
+                this.setState({ shownGallery: <FoodGallery /> })
+            else if (index === 2)
+                this.setState({ shownGallery: <EngagementGallery /> })
+        }
+    }
 
 
     render() {
@@ -20,46 +38,54 @@ class Main extends Component {
             <div>
                 <section id="hero">
                     <div className="container">
-                        
-                            <div className="hero-image">
-                                <div className="hero-text text-light">
-                                    <div className="starter-template">
-                                        <h1 className="font-weight-bold">Amanda's Picturesque Photography</h1>
-                                        <br></br>
-                                        <p className="lead">Welcome to my professional photography page! Here you can peruse through my work and see the world through my eyes. Feel free to contact me about any inquiries.</p>
-                                    </div>
+
+                        <div className="hero-image">
+                            <div className="hero-text text-light">
+                                <div className="starter-template">
+                                    <h1 className="font-weight-bold">Amanda's Picturesque Photography</h1>
+                                    <br></br>
+                                    <p className="lead">Welcome to my professional photography page! Here you can peruse through my work and see the world through my eyes. Feel free to contact me about any inquiries.</p>
                                 </div>
                             </div>
-                        
+                        </div>
+
                     </div>
+
                 </section>
-                <PhotoCarousel />
+
+                <PhotoCarousel imgClick={this.handleImageClick} />
+                {this.state.shownGallery}
                 <br></br>
-                <NatureGallery />
-                <br></br>
-                <EngagementGallery />
-                <br></br>
-                <FoodGallery />
+
                 <section id="about-me" className="grad-dynamic">
                     <div className="container text-center">
+                        <br />
+                        <br />
                         <h3><strong>Amanda Huggakiss</strong></h3>
                         <p></p>
+                        <br />
+                        <br />
                         {/* <br></br> */}
                         <div className="row">
+                            <br />
+                            <br />
                             <div className="col-md-4"></div>
                             <div className="col-md-4">
                                 {/* <div className="amanda-image"></div> */}
                                 <img src="https://cdn.pixabay.com/photo/2014/03/10/17/33/woman-284621_1280.jpg" id="amanda" class="img-circle person" alt="Amanda Huggakiss">
                                 </img>
-                                {/* <br></br> */}
-                                <p><em>Photography takes an instant out of time, altering life by holding it still.</em></p>
+                                <br />
+                                <br />
+                                <br />
+                                <br />
+                                <p><em><strong>"Photography takes an instant out of time, altering life by holding it still."</strong></em></p>
                             </div>
                             <div className="col-md-4"></div>
                         </div>
                         <br></br>
-
                     </div>
                 </section>
+
                 <ContactUs />
                 {/* <Footer /> */}
                 <br></br>
@@ -67,7 +93,7 @@ class Main extends Component {
                 <section className="footer">
 
                 </section>
-            </div>
+            </div >
         )
     }
 }
